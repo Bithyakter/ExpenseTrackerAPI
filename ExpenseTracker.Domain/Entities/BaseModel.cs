bf3f6a8819d7f137b969ea6cad3331066ec54f4e
Mst Bithy <mstbithy.cse@gmail.com>
2022-01-11 12:44:30 +0600

    Add project files.

---
+++ ExpenseTracker.Domain/Entities/BaseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Domain.Entities
+{
+    public class BaseModel
+    {
+        [Column(TypeName = "smalldatetime")]
+        public DateTime? DateCreated { get; set; }
+
+        [Column(TypeName = "smalldatetime")]
+        public DateTime? DateModified { get; set;}
+
+        public bool? IsRowDeleted { get; set;}
+    }
+}
